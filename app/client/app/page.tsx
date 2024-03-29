75917a7eeb78a63d6dbe3757bf257642b2fa1822
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2024-03-29 17:32:12 -0300

    Add Next.js project to Frontend

---
+++ app/client/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen flex justify-center items-center">
+      <h1 className="font-bold text-center text-2xl">En Preparación! :)</h1>
+    </main>
+  );
+}
